d8490da02cebb6e28ce670f31795d472c71873a3
yeliang <81683321@qq.com>
2017-06-09 01:37:00 +0800

    使用 Markdown 自动生成文章目录

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 import markdown
+from markdown.extensions.toc import TocExtension
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.utils.text import slugify
 from comments.forms import CommentForm
 import models
 
@@ -22,18 +24,22 @@ def detail(request, pk):
     # get_object_or_404方法,其作用就是当传入的pk对应的Post在数据库存在时,
     # 就返回对应的post,如果不存在,就给用户返回一个404错误,表明用户请求的文章不存在.
     post = get_object_or_404(models.Post, pk=pk)
-    post.content = markdown.markdown(post.content, 
-                                     extensions=[
+    # markdown.extensions.codehilite代码高亮的拓展
+    # TocExtension(slugify=slugify)Toc自动生成目录,还美化了URL
+    md = markdown.Markdown(extensions=[
                                          "markdown.extensions.extra",
                                          "markdown.extensions.codehilite",
-                                         "markdown.extensions.toc",
-                                     ])
+                                         TocExtension(slugify=slugify),
+                                      ])
+    post.content = md.convert(post.content)
     # 获取评论表单的实例
     form = CommentForm()
     # 获取这篇post下的全部评论
     comment_list = post.comment_set.all()
     # 将文章,表单,以及文章下的评论列表作为模板变量传给detail.html模板,以便渲染相应数据
+    # 添加了一个toc参数,该参数是生成目录的拓展
     context = {"post":post,
+               "toc":md.toc,
                "form":form,
                "comment_list":comment_list}
     return render(request, "blog/detail.html", context=context)
